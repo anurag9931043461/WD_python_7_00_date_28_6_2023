a66cfe2517ef6001a9f29f462ef1b92d03ec3c14
aimartians2 <aimartians2@gmail.com>
2023-08-29 10:44:57 +0530

    commit

---
+++ 1.py
@@ -0,0 +1,7 @@
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+
+user=input("Enter a number:")
+print(user)
---
+++ abc/ab.py
@@ -0,0 +1,32 @@
+# import dhava
+# import lakshman
+
+# dhava.add(10,20)
+# dhava.div(10,20)
+# dhava.multi(10,20)
+# lakshman.sub(10,20)
+
+# import dhava as d
+# import lakshman as l
+# d.add(10,20)
+# d.div(10,20)
+# d.multi(10,20)
+# l.sub(10,20)
+
+# from dhava import *
+# from lakshman import *
+# add(10,20)
+# multi(10,20)
+# div(10,20)
+# sub(10,20)
+
+from krishna.dhava import multi,add,a
+from krishna.lakshman import sub
+
+multi(10,20)
+sub(10,20)
+add(10,20)
+
+import fsdfasf
+fsdfasf.abc()
+print(a)
